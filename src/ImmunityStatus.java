3fb784c3e35af49a0ff8a3b9d08d428b1a2ad442
Danielle <danimiike@gmail.com>
2021-08-01 12:10:23 -0400

    enum added

---
+++ src/ImmunityStatus.java
@@ -0,0 +1,14 @@
+/**
+ * Program Name: ImunityStatus.java
+ * Purpose: put something useful
+ * Coder: Danielle Miike
+ * Date: Aug 1, 2021
+ */
+
+public class ImmunityStatus
+{
+	public enum Status {
+		ONE_SHOT, TWO_SHOT, NO_SHOT, IMMUNE
+	}
+}
+//end class
